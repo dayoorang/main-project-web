55c0aaa0fcc741622496147c8574f94b4dd7b92c
yangsunkang <dayoorang@gmail.com>
2021-11-29 16:35:43 +0900

    주석 추가

--- TEST_JW/test.py
+++ TEST_JW/test.py
@@ -14,7 +14,7 @@ CLIENT_ID = "MawiiHEojSbWlRvZjWEM"
 CLIENT_SECRET = "gY1PNWHP54"
 
 
-def cleanup_text(text):
+def cleanup_text(text): 
     # strip out non-ASCII text so we can draw the text on the image
     # using OpenCV
     return "".join([c if ord(c) < 128 else "" for c in text]).strip()
@@ -31,8 +31,6 @@ def easy_ocr_result(img, language='en', draw=True, text=False):
     text_list = []
 
     for (bbox, text, prob) in results:
-        # display the OCR'd text and associated probability
-        # print("[INFO] {:.4f}: {}".format(prob, text))
         (tl, tr, br, bl) = bbox
         tl = (int(tl[0]), int(tl[1]))
         tr = (int(tr[0]), int(tr[1]))
@@ -49,21 +47,13 @@ def easy_ocr_result(img, language='en', draw=True, text=False):
 def translate_texts(texts: List[str], type='google') -> List[str]:
     global tranlated_texts
 
-    # text_no = len(texts)    
-
-    # if type == 'google':
-    #     translator = googletrans.Translator()
-    #     tranlated_texts = [
-    #         translator.translate(text=text, src='en', dest='ko').text
-    #         for text in texts
-    #     ]
-    if type == 'google':
+    if type == 'google': # 구글 transalotr 이용
         translator = googletrans.Translator()
         tranlated_texts = [
             translator.translate(text=text, src='en', dest='ko').text
             for text in texts
         ]
-    elif type == 'naver':
+    elif type == 'naver': # 네이버 translator 이용
         url = "https://openapi.naver.com/v1/papago/n2mt"
         header = {"X-Naver-Client-Id": CLIENT_ID, "X-Naver-Client-Secret": CLIENT_SECRET}
         tranlated_texts = []
@@ -80,16 +70,16 @@ def translate_texts(texts: List[str], type='google') -> List[str]:
     return tranlated_texts
 
 def cut_image(img, bbox):
-    x_min = bbox[0, 0]
-    x_max = bbox[1, 0]
-    y_min = bbox[0, 1]
-    y_max = bbox[2, 1]
+    x_min = bbox[0, 0]  # 좌상단 
+    x_max = bbox[1, 0]  # 좌하단 
+    y_min = bbox[0, 1]  # 우상단
+    y_max = bbox[2, 1]  # 우하단
 
-    img = img[y_min:y_max, x_min:x_max]
+    img = img[y_min:y_max, x_min:x_max] # 바운딩 박스 좌표를 통해 바운딩박스 크기의 이미지를 뽑아내는 과정
 
     return img
 
-def rgb(img):
+def rgb(img): 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     _, mask = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
 
@@ -138,24 +128,33 @@ def decsion_font_size(bbox_hi, text):
     return font_size
 
 def change_color(img):
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # gray 영상으로 만들기
-    ret, img_binary = cv2.threshold(img_gray, 0, 255, cv2.THRESH_OTSU) # 마스킹
-
-    if len(img_binary[img_binary > 250]) > len(img_binary[img_binary < 250]): 
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # 이진 영상으로 만들기 전 grayscale 영상으로 변경
+    ret, img_binary = cv2.threshold(img_gray, 0, 255, cv2.THRESH_OTSU) # otsu 기법을 통해 grascaled된 영상으로 검은색과 흰색으로 이루어진 이진영상으로 변경
+    # return 된 ret는 OTSU기법을 통해 계산된 threshold 이고 , img_binary는 이진영상이다.
+    if len(img_binary[img_binary > 250]) > len(img_binary[img_binary < 250]):
+        # 우리가 실제로 필요한 이진 영상의 형태는 글자(객체)는 하얀색, 배경은 검은색이다.
+        # 하지만 영상에 따라 글자가 더 어두울 경우 글자를 검은색으로 분류를 하는 경우가 발생한다.
+        # 이를 해결하기위해 임의의 숫자 250(하얀색에 가까운 수)를 기준으로 위와 같은 if문을 지정하였고 if문에 해당될 경우 반전하는 코드인 bitwise_not을 사용하였다. 
+        # len(img_binary[img_binary > 250]) ==> 250보다 큰 픽셀값이 이미지에 몇개있는지를 체크 즉 하얀색 픽셀 갯수 체크
+        # len(img_binary[img_binary < 250]) ==> 250보다 작은 픽셀값 체크 즉 검은색 픽셀 갯수 체크
+        # 이 두값중 "len(img_binary[img_binary > 250])"이 더크다는 것은 배경이 하얀색이라는 의미임으로 반전을 해야한다.(전제: 객체보다 배경의 픽셀수가 더 많다 가정)
         img_binary = cv2.bitwise_not(img_binary)
 
-    masked = cv2.bitwise_and(img, img, mask = img_binary)
+    masked = cv2.bitwise_and(img, img, mask = img_binary) # 객체부분만 가져오기위한 코드
 
 
-    b, g, r = cv2.split(masked)
-    b, g, r = int(np.mean(b[b > 0])), int(np.mean(g[g > 0])), int(np.mean(r[r > 0]))
+    b, g, r = cv2.split(masked) # 객체를 split함수를 통해 b, g, r 각각의 컬러로 분리
+    b, g, r = int(np.mean(b[b > 0])), int(np.mean(g[g > 0])), int(np.mean(r[r > 0])) 
+    # 글자(객체)의 컬러를 보면 solid 하기보단 그라데이션이 적용되기도하고 불투명한 경우도 많다.
+    # 이러한 컬러 특징을 제대로 잡아내기는 현실적으로 어려움이 있어 컬러의 평균을 구해 b, g, r 로 재지정
 
     return b,g,r
 
-def change_bg_color(img):
 
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # gray 영상으로 만들기
-    ret, img_binary = cv2.threshold(img_gray, 0, 255, cv2.THRESH_OTSU) # 마스킹
+def change_bg_color(img):
+    # 위 change_color와 같은 방법으로 역을 구해 background 컬러를 구하는 과정
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+    ret, img_binary = cv2.threshold(img_gray, 0, 255, cv2.THRESH_OTSU) 
     bg_binary = cv2.bitwise_not(img_binary)
 
     if len(img_binary[img_binary > 250]) > len(img_binary[img_binary < 250]): 
@@ -166,32 +165,47 @@ def change_bg_color(img):
     b, g, r = cv2.split(masked_bg)
     b, g, r = int(np.mean(b[b > 0])), int(np.mean(g[g > 0])), int(np.mean(r[r > 0]))
     
-    a = np.ones(shape=img.shape, dtype = np.uint8)
-    b = a[:,:,0] * b
+
+    a = np.ones(shape=img.shape, dtype = np.uint8)  # change_color와는 다르게 boundingbox 전체를 하나의 컬러로 덮는 용도이기 때문에 컷팅된 이미지의 shape크기만큼의 이미지를 생성
+    b = a[:,:,0] * b # 각각의 컬러 만큼 곱해주고 b, g, r에 담는다.
     g = a[:,:,1] * g
     r = a[:,:,2] * r
 
     
     return b,g,r
 
+'''
+rewrite 함수는 원본 이미지에서 원래 글자가 있는 부분을 지운 형태의 이미지를 첫번째 인자로 전달 받음,
+해당함수의 목적은 원본 이미지에 번역 된 글자를 덮어 쓰기 위함
+'''
 def rewrite(img, tranlated_texts ,bbox_list, color_list):
     img = img
     image_editable = ImageDraw.Draw(img)
 
     ### font size ###
+    '''' 
+    원본이미지의 폰트사이즈를 이용하여 번역된 글자 폰트 사이즈를 정하는 코드
+    원본이미지의 폰트사이즈는 해당 글자(객체) 바운딩 박스 높이를 기준으로 하였으나, 같은 크기의 글자임에도 바운딩박스 크기에 따라 
+    글자 크기가 변하는 문제 발생
+
+    이 문제를 해결 하기 위해 각 바운딩박스의 median값을 구하고 median값과 차이가 큰 값들은 특별히 작거나 큰 값이므로, 원래 값을 그대로 이용하고
+    차이가 작은 값들은 모두 같은 크기(여기선 median값)으로 변경함으로써 문제 해결
+    '''
     bbox_hi = []
     for bbox in  bbox_list:
         bbox_hi.append(bbox[2][1] - bbox[0][1]) 
+    # 위 문제를 해결하기 위해 각 바운딩박스의 높를 리스트에 담음
 
-    bbox_hi_median = int(np.median(bbox_hi))
-    bbox_hi_median_diff = np.abs(np.array(bbox_hi) - bbox_hi_median ) # 절대값 추출
+
+    bbox_hi_median = int(np.median(bbox_hi)) # 리스트에 담은 값들의 중앙값 추출
+    bbox_hi_median_diff = np.abs(np.array(bbox_hi) - bbox_hi_median ) # 각각의 값들이 중앙 값에서 얼마나 벗어나있는지를 알기 위한 코드
     print('bbox_hi_median',bbox_hi_median)
-    hi_lt_idx = np.where(bbox_hi_median_diff < 10) # 10보다 적게나는 값 idx 추출
+    hi_lt_idx = np.where(bbox_hi_median_diff < 10) # 10보다 적게나는 값 idx 추출(10은 테스트를 통해 가장 좋은 값으로 정함)
 
     # bbox_hi > array 변경
-    bbox_hi = np.array(bbox_hi)
-    # 차이 작은 값은 median값으로 변경 큰것은 그대로.
-    bbox_hi[hi_lt_idx] = bbox_hi_median
+    bbox_hi = np.array(bbox_hi) # 계산 편의를 위해 넘파이로 변경
+    bbox_hi[hi_lt_idx] = bbox_hi_median   # 차이가 작은 값은 median값으로 변경하고 큰것은 원래 값 그대로 사용.
+
 
     print('bbox_hi', bbox_hi)
     for idx, (bbox,color) in enumerate(zip(bbox_list,color_list)):  
@@ -199,7 +213,9 @@ def rewrite(img, tranlated_texts ,bbox_list, color_list):
         print('fontsize',bbox_hi[idx]-15)
         text = tranlated_texts[idx]
 
-        title_font = ImageFont.truetype("ttf/NotoSansKR-Bold.otf", np.maximum(2, bbox_hi[idx]-10)) # -가 될경우 최소 2로 설정.
+        title_font = ImageFont.truetype("ttf/NotoSansKR-Bold.otf", np.maximum(2, bbox_hi[idx]-5)) 
+        # ImageFont.truetype 두번째 인자가 폰트 사이즈가 들어가는 위치인데, -5 정도를 빼줘야 크기가 비슷함
+        # 다만 크기가 작은 텍스트일 경우 -가 되버리는 경우가 있기 때문에 최소 크기 2가 되도록 고정
         wi, hi = title_font.getsize(text)
 
         print('bbox_hi[idx]-15', type(bbox_hi[idx]-15))
@@ -210,8 +226,6 @@ def rewrite(img, tranlated_texts ,bbox_list, color_list):
         image_editable.text((start_x, start_y), text, color, anchor = 'lt', font=title_font)
 
     return img
-    # print('img type',type(img))
 
-    # save_rewrite_images(img)
 
 
--- inpainting/models.py
+++ inpainting/models.py
@@ -18,60 +18,73 @@ from TEST_JW.test import (
     change_bg_color,
 )
 
+'''
+변경된 이미지 출력 과정 :
+---> 
+1. Image 필드를 통해 image를 전달 받은 후, 
+내부 알고리즘을 이미지를 수정하고 수정된 이미지를 DB상에 저장
+2. 이미지를 웹페이지 출력할때는 DB에서 변경된 이미지를 불러오는식으로 진행
+'''
 
 class Image(models.Model):
     image = models.ImageField(upload_to='image/',blank=True, null=True)
 
-    def save(self, *args, **kwargs):
-        # 이미지 열기
+    def save(self, *args, **kwargs): # save 모델을 수정하여 1번 과정을 수행
 
-
-        img_pil = PIL.Image.open(self.image).convert('RGB') # 4channel 들어올경우 -> 3channl 
+        img_pil = PIL.Image.open(self.image).convert('RGB') 
+        # 우리가 사용할 easy-ocr모델에는 4채널 이미지가 들어올 경우 오류가 발생함으로 강제적으로 3채널 이미지로 제한
 
 
         img_np = np.array(img_pil)
-
-        # # 3차원으로 변경
-        # img_np = cv2.imread(img_pil, cv2.IMREAD_COLOR)
+        # 이미지 수정/변경 편의를 위해 넘파이 array 형태로 변경
 
         print('img_np shape', img_np.shape)
         bbox_list, text_list = easy_ocr_result(img_np)
+        # 넘파이 array형태의 이미지를 easy_ocr_result 함수의 인자로 사용
+        # 함수 내부에서 이미지안에 있는 text와, bounding box 위치를 리스트 형태로 각각 반환.
 
-        tranlated_texts = translate_texts(texts=text_list, type='naver')
-
-
-        color_list=[]
         
+        tranlated_texts = translate_texts(texts=text_list, type='naver')
+        # translate_texts에 위에서 return 받은 text_list를 전달받게 되는데, 이는 각 리스트들을 한글로 재번역하기 위함,
+        # 두번째 인자로 type='naver'은 naver translator api를 사용하기 위한 코드이다.
+
+
+
+        ## 각각의 text 객체들을 개별로 수정하기 위해 easy_ocr_result 함수에서 얻은 bounding box 리스트를 for loop을 통해 하나씩 뽑아낸다. ## 
+        color_list=[]
         for bbox in bbox_list:
-            img_cut = cut_image(img_np, bbox)
+            img_cut = cut_image(img_np, bbox) # cut_image 함수의 첫번째 넘파이 이미지 array를, 두번째 인자로는 바운딩박스가 들어가게 된다.
+            # for loop을 통해 얻은 각각의 바운딩 박스를 통해 전체 이미지에서 글자만 있는 부분을 cutting 하게되고 이를 통해 얻은 값은 img_cut 이라는 변수에 담았다.
 
-            color_list.append(change_color(img_cut))
+            color_list.append(change_color(img_cut)) # cutting 된 이미지는 change_color 라는 함수에 인자로 넣는데, 이는 이미지의 포함되어있는 글자의 컬러를 뽑아내고,
+            # 각 객체의 컬러들을 리스트에 담는 코드이다.
 
-            # mask = mask_image(img_cut)
-            # masked_img = cv2.inpaint(img_cut, mask, 3, cv2.INPAINT_TELEA)
             print('img_cut shape',img_cut.shape)
-            b,g,r = change_bg_color(img_cut)
-            print(b.shape)
+            b,g,r = change_bg_color(img_cut) # 각 cutting된 이미지의 배경 컬러를 뽑아내는 코드이다.(자세한 설명은 test.py 코드 설명 참조)
+
+            # 원본이미지에 수정된 이미지를 덮어 글자를 지운다.
             img_cut[:,:,0] = b
             img_cut[:,:,1] = g
             img_cut[:,:,2] = r
 
+
             img_np = change_original(img_np, img_cut, bbox)
+            # change_original 함수의 첫번째 인자로는 원본이미지, 두번째 인자로는 수정된 커팅 이미지, 세번째 인자로는 바운딩박스가 들어간다.
+            # 각각의 인자를 이용해 원본 이미지를 for loop 순으로 순차적으로 수정해 나간다.
+            # 여기 까지 코드는 원본 이미지에 있는 글자를 덮는 기능이다.
+
+
 
         print('color list',color_list)
 
         img_pil = PIL.Image.fromarray(img_np)
-        # print('type',type(img_pil))
-        img = rewrite(img_pil, tranlated_texts,bbox_list, color_list)
+        # 수정된 넘파이 array 이미지를 다시 Pillow 이미지 형태로 변경한다.(글자를 이미지에 적을 pillow 함수를 사용하기 위함)
 
-        # print(img)
-        # print(type(img))
-        #  convert back to pil image
-        # im_pil = Image.fromarray(img)
-        # plt.imshow(img)
-        # plt.show()
-        # 저장
-        
+        img = rewrite(img_pil, tranlated_texts,bbox_list, color_list)
+        # rewrite 함수의 첫번째 인자로는 pillow형태의 이미지, 두번째 인자로는 번역된 글(한글) 리스트, 세번째 인자로는 바운딩박스 리스트, 세번째로는 각 글자의 컬러를 담은 리스트이다.
+        # rewrite 를 통해 나온 최종 수정된 이미지는 배경이 지워지고, 원본 글자(영어)의 컬러를 따서 원래 위치에 넣어준 이미지이다.
+
+        # 최종 수정된 이미지를 장고가 읽을 수있도록 변경 하는 코드
         buffer = BytesIO()
         img.save(buffer, format='png')
         image_png = buffer.getvalue()
